b08b7f4705537870054d21fe2c492dcb418d052b
acandael <info@anthonycandaele.com>
2019-02-04 14:10:11 +0100

    implement game reset

--- js/app.js
+++ js/app.js
@@ -2,6 +2,27 @@ const startButton = document.getElementById('btn__reset');
 const game = new Game();
 
 startButton.addEventListener('click', function() {
+  // remove all li elements from the phrase
+  const phraseDisplay = document.querySelectorAll('#phrase li');
+  phraseDisplay.forEach(item => {
+    item.remove();
+  });
+
+  // enable all the onscreen keyboard buttons
+  const keyboard = document.querySelectorAll('.keyrow button');
+
+  keyboard.forEach(key => {
+    key.setAttribute('class', 'key');
+  });
+
+  // reset all the heart images
+  const lives = document.querySelectorAll('.tries');
+
+  lives.forEach(live => {
+    let img = live.firstChild;
+    img.setAttribute('src', 'images/liveHeart.png');
+  });
+
   game.startGame();
 });
 
